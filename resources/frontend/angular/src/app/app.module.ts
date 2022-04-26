876081d976d1aa319427053b611a5d349f7ddfa9
ranaibraheem <rana.ibraheem82@gmail.com>
2022-04-26 16:31:08 +0200

    home update

--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +12,20 @@ import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
 import { SaleComponent } from './sale/sale.component';
 import { TopComponent } from './top/top.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'body', component:BodyComponent },
+  { path: 'top', component:TopComponent },
+  { path: 'sale', component:SaleComponent },
+  { path: 'navbar', component:NavbarComponent },
+  { path: 'footer', component:FooterComponent },
+  // { path: '', redirectTo: '/body', pathMatch: 'full' },
+
+
+
+]
 
 @NgModule({
   declarations: [
@@ -19,13 +34,15 @@ import { TopComponent } from './top/top.component';
     FooterComponent,
     BodyComponent,
     SaleComponent,
-    TopComponent
+    TopComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]

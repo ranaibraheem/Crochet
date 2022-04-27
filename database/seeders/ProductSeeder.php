ebfbe3eda2c5a7eec5e0fc25c36d3c6fe5823c49
ranaibraheem <rana.ibraheem82@gmail.com>
2022-04-27 22:36:28 +0200

    new component & migration table

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('products')->insert([
+            'name'          => 'Crochet 1',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet1.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 2',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet2.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 3',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet3.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 4',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet4.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 5',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet5.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 6',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet6.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 7',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet7.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 8',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet8.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 9',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet9.jpg',
+        ]);
+        DB::table('products')->insert([
+            'name'          => 'Crochet 10',
+            'description'   => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, expedita?',
+            'price'         => '10.00',
+            'image'         => 'crochet10.jpg',
+        ]);
+
+    }
+}
--- resources/frontend/angular/src/app/new/new.component.ts
+++ resources/frontend/angular/src/app/new/new.component.ts
@@ -6,8 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
-
-  constructor() { }
+  imagePath="assets/images/";
+  newCollections = [
+    {
+      id: 1,
+      name: 'Item 1',
+      image: 'crochet75.jpg',
+      price: '43.00'
+    },
+    {
+      id: 2,
+      name: 'Item 2',
+      image: 'crochet42.jpg',
+      price: '36.00'
+  },
+  {
+      id: 3,
+      name: 'Item 3',
+      image: 'crochet24.jpg',
+      price: '50.00'
+  },
+  {
+      id: 4,
+      name: 'Item 4',
+      image: 'crochet46.jpg',
+      price: '45.00'
+  },
+  {
+      id: 5,
+      name: 'Item 5',
+      image: 'crochet12.jpg',
+      price: '60.00'
+  },
+  {
+      id: 6,
+      name: 'Item 6',
+      image: 'crochet27.jpg',
+      price: '65.00'
+  }
+  ]
+    constructor() { }
 
   ngOnInit(): void {
   }

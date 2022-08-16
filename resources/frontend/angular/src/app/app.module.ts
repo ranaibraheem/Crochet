a60ddbdb48ecac2f8523758cb0d7ddba69e77319
ranaibraheem <rana.ibraheem82@gmail.com>
2022-08-16 08:49:19 +0200

    components updat

--- resources/frontend/angular/src/app/app.module.ts
+++ resources/frontend/angular/src/app/app.module.ts
@@ -5,22 +5,20 @@ import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-import { SaleComponent } from './sale/sale.component';
-import { TopComponent } from './top/top.component';
 import { HomeComponent } from './home/home.component';
 import { CollectionsComponent } from './collections/collections.component';
-import { NewComponent } from './new/new.component';
 import { DetailComponent } from './detail/detail.component';
+import { CrochetComponent } from './crochet/crochet.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'collections', component:CollectionsComponent },
-  { path: 'new', component:NewComponent },
   { path: 'detail/:id', component:DetailComponent },
 ]
 
@@ -30,16 +28,34 @@ const appRoutes: Routes = [
     NavbarComponent,
     FooterComponent,
     BodyComponent,
-    SaleComponent,
-    TopComponent,
     HomeComponent,
     CollectionsComponent,
-    NewComponent,
     DetailComponent,
+    CrochetComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {
+      // In order to get anchor / fragment scrolling to work at all, we need to
+      // enable it on the router.
+      // anchorScrolling: "enabled",
+
+      scrollOffset: [0, 0],
+      // Once the above is enabled, the fragment link will only work on the
+      // first click. This is because, by default, the Router ignores requests
+      // to navigate to the SAME URL that is currently rendered. Unfortunately,
+      // the fragment scrolling is powered by Navigation Events. As such, we
+      // have to tell the Router to re-trigger the Navigation Events even if we
+      // are navigating to the same URL.
+
+      // onSameUrlNavigation: "reload",
+
+      // Let's enable tracing so that we can see the aforementioned Navigation
+      // Events when the fragment is clicked.
+
+      // enableTracing: true,
+      scrollPositionRestoration: 'top' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(appRoutes)

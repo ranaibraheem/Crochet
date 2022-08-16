9819fa31c3c604ef513fae95b09ac26b3dcc705a
ranaibraheem <rana.ibraheem82@gmail.com>
2022-08-16 15:34:35 +0200

    pagination&detail&others

--- resources/frontend/angular/src/app/body/body.component.ts
+++ resources/frontend/angular/src/app/body/body.component.ts
@@ -20,45 +20,13 @@ export class Collection{
 export class BodyComponent implements OnInit {
 
   imagePath="assets/images/";
-  detailPath="detail/:id";
+  detailPath="detail/";
   items:Collection[] = [];
 
   constructor(
     private httpClient: HttpClient,
   ) {}
 
-  // newCollections = [
-  //   {
-  //     id: 1,
-  //     name: 'Item 1',
-  //     image: 'crochet50.jpg',
-  //     price: '43.00'
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Item 2',
-  //     image: 'crochet42.jpg',
-  //     price: '36.00'
-  // },
-  // {
-  //     id: 3,
-  //     name: 'Item 3',
-  //     image: 'crochet27.jpg',
-  //     price: '50.00'
-  // },
-  // {
-  //     id: 4,
-  //     name: 'Item 4',
-  //     image: 'crochet46.jpg',
-  //     price: '45.00'
-  // },
-  // {
-  //     id: 5,
-  //     name: 'Item 5',
-  //     image: 'crochet24.jpg',
-  //     price: '65.00'
-  // }
-  // ]
 
   ngOnInit(): void {
     this.getCollections();
--- resources/frontend/angular/src/app/detail/detail.component.ts
+++ resources/frontend/angular/src/app/detail/detail.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+// import { environment } from 'src/environments/environment';
+
+export class Collection{
+  constructor(
+    public id:number,
+    public name:string,
+    public location:string,
+    public image:string,
+  ){
+  }
+}
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +20,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  itemId:any;
+  items:Collection[] = [];
+  showModal: boolean=false;
+  imagePath:string = 'assets/images/';
+
+  constructor(
+    private httpClient: HttpClient,
+    private route: ActivatedRoute
+  ) {
+
+  }
 
   ngOnInit(): void {
+    this.getCollections();
+    this.route.params.subscribe(params => {
+      this.itemId = +(params['id']);
+    })
   }
+
+   getCollections(){
+    this.httpClient.get<any>('http://127.0.0.1:8000/api/products').subscribe(Response => {
+    this.items = Response.data;
+    });
+  }
+  showHide()
+  {
+    this.showModal = !this.showModal;
+  }
+ ClickedOut(event:any) {
+    if(event.target.className == "hover_bkgr") {
+      this.showModal = false;
+    }
+ }
+
 
 }
